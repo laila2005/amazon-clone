a422d2b29219ed74d342b55eb9ec25fdcb6cbd35
laila2005 <laila.mf2005@gmail.com>
2025-08-16 14:20:37 +0300

    feat: update react-router-dom to version 7.8.1 and refactor App component to use Router and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Add more routes here, e.g. <Route path="/about" element={<AboutPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
